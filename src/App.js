e71909bd6259113a76fda1777bd0614b158b954f
Jennifer Garst <jennifer.garst@stjude.org>
2020-12-23 15:11:07 -0600

    Fix styling and change up colors

--- src/App.js
+++ src/App.js
@@ -1,49 +1,36 @@
-import "./App.css";
-
 function App() {
   return (
-    <div className="fluid-container d-flex vh-100 flex-column justify-content-between align-items-center">
-      <div className="d-flex align-items-center" id="header">
-        <header>To Do List</header>
+    <>
+      <header>To Do List</header>
+      <div id="middle">
+        <div id="to-do">
+          <form>
+            <div className="task-item">
+              <input type="checkbox" id="1" />
+              <label htmlFor="1">Clean living room</label>
+            </div>
+            <div className="task-item">
+              <input type="checkbox" id="2" />
+              <label htmlFor="2">Clean bathroom</label>
+            </div>
+            <div className="task-item">
+              <input type="checkbox" id="3" />
+              <label htmlFor="3">Make bed</label>
+            </div>
+            <div className="task-item">
+              <input type="checkbox" id="4" />
+              <label htmlFor="4">Do laundry</label>
+            </div>
+            <div className="task-item last-item">
+              <input type="checkbox" id="5" />
+              <label htmlFor="5">Make lunch</label>
+            </div>
+          </form>
+          <div id="controls"></div>
+        </div>
       </div>
-      <div className="d-flex justify-content-start" id="to-do">
-        <form className="d-flex flex-column">
-          <div className="form-group">
-            <input type="checkbox" id="1" className="form-check-input" />
-            <label htmlFor="1" className="form-check-label">
-              Clean living room
-            </label>
-          </div>
-          <div className="form-group">
-            <input type="checkbox" id="2" className="form-check-input" />
-            <label htmlFor="2" className="form-check-label">
-              Clean bathroom
-            </label>
-          </div>
-          <div className="form-group">
-            <input type="checkbox" id="3" className="form-check-input" />
-            <label htmlFor="3" className="form-check-label">
-              Make bed
-            </label>
-          </div>
-          <div className="form-group">
-            <input type="checkbox" id="4" className="form-check-input" />
-            <label htmlFor="4" className="form-check-label">
-              Do laundry
-            </label>
-          </div>
-          <div className="form-group">
-            <input type="checkbox" id="5" className="form-check-input" />
-            <label htmlFor="5" className="form-check-label">
-              Make lunch
-            </label>
-          </div>
-        </form>
-      </div>
-      <div className="d-flex align-items-center" id="footer">
-        &copy;2020 | Jennifer Garst
-      </div>
-    </div>
+      <footer>&copy;2020 | Jennifer Garst</footer>
+    </>
   );
 }
 
